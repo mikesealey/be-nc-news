8be2fef336ef7f4965d3b75a2281fa5b9c934e85
Mike <mikesealey@hotmail.com>
2023-06-27 15:09:52 +0100

    Solves ticket 4 with test and improved psql error handling without console.logs

--- errors.js
+++ errors.js
@@ -1,5 +1,4 @@
 exports.psqlErrorHandler = (err, req, res, next) => {
-    console.log("psqlerrorhandler called")
     if (err.code === "22P02") {
         res.status(400).send({msg: "Bad Request"})
     } else res.status(500).send({ msg: 'Internal Server Error' })
--- models/api.models.js
+++ models/api.models.js
@@ -35,19 +35,14 @@ exports.selectApiEndPoints = () => {
 }
 
 exports.selectArticleById = (id) => {
-    console.log(id)
-
-
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [id])
     .then(({rows}) => {
-        console.log(">>>>>>>>>>>",rows)
         if (rows.length === 0) {
             return Promise.reject({status: 404, msg: "Not found"})
         } else {
-            console.log(rows)
             return rows[0]
         }
-        
+
     })
 }
 
